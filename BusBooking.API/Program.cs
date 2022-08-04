5d3f166209656bb1c329d786ed779db25c13d2a3
Mayur Khairkar <95740994+mkhairkar@users.noreply.github.com>
2022-08-04 14:54:25 +0530

    Update Program.cs

--- BusBooking.API/Program.cs
+++ BusBooking.API/Program.cs
@@ -11,7 +11,10 @@ namespace BusBooking.API
 {
     public class Program
     {
-        
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
